f4cf9d0e8ee26e14cbcd1857e267cc29b846f3a2
sudo-alex1990 <alexrfarnes@gmail.com>
2021-04-21 10:26:06 +0800

    Add a dollar sign to the income/expenses UI

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function addTransactionDOM(transaction) {
     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
     item.innerHTML = `
-        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class="delete-btn" onclick="deleteEntry(${transaction.id})">x</button>
+        ${transaction.text} <span>${sign}$${Math.abs(transaction.amount)}</span> <button class="delete-btn" onclick="deleteEntry(${transaction.id})">x</button>
     `;
 
     list.appendChild(item);
